6c7f9b512660b4ab32b1b64b928c2a902798fe50
zaysevkun <zaysev100@gmail.com>
2020-11-19 23:51:53 +1000

    models for event and organization

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Organization(models.Model):
+    name = models.CharField('Название организации', max_length=64)
+    description = models.TextField('Описание')
+    mission = models.TextField('Миссия')
+    motivation = models.TextField('Мотивировка')
+    work_trajectory = models.TextField('Траектория работы')
+    goal = models.TextField('Цель')
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField('Название мероприятия', max_length=64)
+    organization = models.ForeignKey(Organization, verbose_name="Организатор мероприятия")
+    date = models.DateField('Дата проведения')
+
+    def __str__(self):
+        return self.name
