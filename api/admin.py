83bd844aea2b1e8ae665aadd34014370c2491767
zaysevkun <zaysev100@gmail.com>
2020-12-02 00:27:09 +1000

    fixed migrations

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
-from .models import Event, Organization, Inventory
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import Event, Organization, Inventory, User
 
 admin.site.register(Event)
 admin.site.register(Organization)
 admin.site.register(Inventory)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('full_name',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('email', 'full_name')
+    search_fields = ('email', 'full_name', )
+    ordering = ('email',)
